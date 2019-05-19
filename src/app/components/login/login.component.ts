7bc0ab20aa24d090534f89f7f86aee264041bd88
Joseochoa227 <joseochoa227@gmail.com>
2019-05-19 16:04:05 -0400

    Login y autenticacion de usuario

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,17 +1,80 @@
 import { Component, OnInit } from '@angular/core';
-
+import { User } from '../../models/user';
+import { UserService } from '../../services/user.service';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 @Component({
   selector: 'login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [UserService]
 })
 export class LoginComponent implements OnInit {
-  public page_title:string;
-  constructor() {
+  public page_title: string;
+  public user: User;
+  public identity;
+  public token: string;
+  public status: string;
+  constructor(
+    private _userService: UserService,
+    private _router: Router,
+    private _route: ActivatedRoute
+  ) {
     this.page_title = 'Login';
-   }
+    this.user = new User(1, '', '', '', '', 'ROLE_USER', '');
+  }
 
   ngOnInit() {
+    this.logout();
+  }
+  onSubmit(form) {
+    this._userService.signup(this.user).subscribe(
+      response => {
+        if (!response.status || response.status != 'error') {
+          this.status = 'success';
+          this.identity = response;
+
+          //Una vez tengo los datos del usuario Saco mi token
+          this._userService.signup(this.user, true).subscribe(
+            response => {
+              if (!response.status || response.status != 'error') {
+
+                this.token = response;
+
+                localStorage.setItem('token', this.token);
+                localStorage.setItem('identity', JSON.stringify(this.identity));
+                this._router.navigate(['inicio'])
+
+              } else {
+                this.status = 'error';
+              }
+            },
+            error => {
+              console.log(error);
+              this.status = 'error';
+            }
+          );
+        } else {
+          this.status = 'error';
+        }
+      },
+      error => {
+        console.log(error);
+        this.status = 'error';
+      }
+    );
+  }
+  logout(){
+    this._route.params.subscribe(params => {
+        let sure =  +params['sure'];
+        if(sure == 1){
+          localStorage.removeItem('identity');
+          localStorage.removeItem('token');
+
+          this.identity= null;
+          this.token=null;
+          this._router.navigate(['inicio']);
+        }
+    });
   }
 
 }
