1da50d1e49206d36a28da2d44deee71672629b80
Joseochoa227 <joseochoa227@gmail.com>
2019-05-19 12:37:26 -0400

    Maquetado basico, creacion de componentes y rutas

---
+++ src/app/app.routing.ts
@@ -0,0 +1,20 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule, Router } from '@angular/router';
+
+//Componentes
+import { HomeComponent } from './components/home/home.component';
+import { ErrorComponent } from './components/error/error.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+
+const appRoutes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'inicio', component: HomeComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'registro', component: RegisterComponent},
+    {path: 'error', component: ErrorComponent},
+    {path: '**', component: ErrorComponent}
+];
+
+export const appRoutingProviders: any[] = [];
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
