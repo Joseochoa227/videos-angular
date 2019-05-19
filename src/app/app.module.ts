005458dc17d2450db0ef2b21d0deec2f2b2cb850
Joseochoa227 <joseochoa227@gmail.com>
2019-05-19 14:12:00 -0400

    Terminado el componente de registro de usuario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {routing, appRoutingProviders } from './app.routing';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -18,7 +21,9 @@ import { LoginComponent } from './components/login/login.component';
   ],
   imports: [
     BrowserModule,
-    routing
+    routing,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [
     appRoutingProviders
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,17 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/user';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
+  providers: [UserService]
 })
 export class RegisterComponent implements OnInit {
   public page_title: string;
-  constructor() { 
+  public user: User;
+  public status: string;
+  constructor(
+      private _userService: UserService
+  ) { 
     this.page_title = 'Registro';
+    this.user= new User(1,'','','','','ROLE_USER','');
   }
 
   ngOnInit() {
+      console.log(this._userService.prueba());
+  }
+  onSubmit(form){
+    this._userService.register(this.user).subscribe(
+        response =>{
+            if(response.status == 'success'){
+                this.status= 'success';
+                form.reset();
+            }else{
+                this.status = 'error';
+            }
+        },
+        error   =>{
+          this.status = 'error';
+          console.log(error);
+        }
+    );
   }
 
 }
